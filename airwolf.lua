b32ba55d3314838c2b1a52c813e15485c167d528
Zeeshan Ali Khan <zeenix@gmail.com>
2025-01-04 21:16:41 +0100

    Add theme music from Boris Milmeister & Leonard Milmeister
    It's a bit off-beat atm but that's my fault and we'll fix it.

--- airwolf.lua
+++ airwolf.lua
@@ -3,6 +3,8 @@ function _init()
 
     airwolf = Aircraft:airwolf()
     enemy = Aircraft:enemy_heli_small()
+
+    music(0)
 end
 
 function _update()
@@ -10,6 +12,10 @@ function _update()
     if enemy:update() then
         -- For now just respawn the enemy.
         enemy = Aircraft:enemy_heli_small()
+    end
+
+    if stat(54) == -1 then
+       music(2, 200)
     end
 end
 
